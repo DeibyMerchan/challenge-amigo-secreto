9c9105eddd9b0dfc87aa41edc854e3342c5ad58a
Deiby Merchan <est.deibymerchan@gmail.com>
2025-02-02 12:35:44 -0500

    Update list in the index

--- app.js
+++ app.js
@@ -1,24 +1,38 @@
-//varible array
-const listaDeAmigos = [];
+// Varible array
+const listaAmigos = [];
 
-// agrega un amigo a la array
+
+// Agrega un amigo a la array
 function agregarAmigo(){
     let amigoIngresado = document.getElementById('amigo').value;
     console.log(amigoIngresado);
     
-    if (isNaN(amigoIngresado)){
-        listaDeAmigos.push(amigoIngresado);
-        limpiarCaja();
-    } else {
+    if (amigoIngresado == ''){
         alert('El valor ingresado no es valido');
-        limpiarCaja();
+        limpiarCaja('amigo');
+    } else {
+        listaAmigos.push(amigoIngresado);
+        enlistar();
+        limpiarCaja('amigo');
+
         }
-    return console.log(listaDeAmigos);
+    return console.log(listaAmigos);
+}
+
+// Enlista los nombres ingresados
+function enlistar(){
+    // Inicia una lista HTML desordenada
+    let text = "<ul>";
+    // Recorre el array
+    listaAmigos.forEach((amigo) => {
+        text += `<li> ${amigo} </li>`;
+    });
+    // Cierra la lista
+    text += "</ul>";
+    document.getElementById('listaAmigos').innerHTML = text;
 }
 
 // Limpia la caja
-function limpiarCaja(){
-    document.getElementById('amigo').value = '';
+function limpiarCaja(elemento){
+    document.getElementById(elemento).value = '';
 }
-
-
