d15d98cbb9fe73a2efd08f2aedc0a08706b07b3b
Deiby Merchan <est.deibymerchan@gmail.com>
2025-02-01 21:48:53 -0500

    creacion de la array

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//varible array
+const listaDeAmigos = [];
+
+// agrega un amigo a la array
+function agregarAmigo(){
+    let amigoIngresado = document.getElementById('amigo').value;
+    console.log(amigoIngresado);
+    
+    if (isNaN(amigoIngresado)){
+        listaDeAmigos.push(amigoIngresado);
+        limpiarCaja();
+    } else {
+        alert('El valor ingresado no es valido');
+        limpiarCaja();
+        }
+    return console.log(listaDeAmigos);
+}
+
+// Limpia la caja
+function limpiarCaja(){
+    document.getElementById('amigo').value = '';
+}
+
+
