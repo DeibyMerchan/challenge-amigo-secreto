ba367a9ac71d0fb7918bc2280e725abe50e191d3
Deiby Merchan <est.deibymerchan@gmail.com>
2025-02-02 21:13:38 -0500

    Js finalizada

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 // Varible array
 const listaAmigos = [];
-
-
 // Agrega un amigo a la array
 function agregarAmigo(){
     let amigoIngresado = document.getElementById('amigo').value;
@@ -18,7 +16,6 @@ function agregarAmigo(){
         }
     return console.log(listaAmigos);
 }
-
 // Enlista los nombres ingresados
 function enlistar(){
     // Inicia una lista HTML desordenada
@@ -31,7 +28,19 @@ function enlistar(){
     text += "</ul>";
     document.getElementById('listaAmigos').innerHTML = text;
 }
-
+// Elige aleatoriamente un valor en el array
+function sortearAmigo(){
+        // Genera un numero entre 0 y el ultimo valor indice de la array
+        let rango = Math.floor(Math.random() * listaAmigos.length);
+        // Variable donde se alamacena el indice sorteado
+        let amigoSorteado = listaAmigos[rango];
+        
+        console.log(amigoSorteado);
+        // Muestra el valor sorteado
+        document.getElementById("resultado").innerHTML = `El amigo secreto sorteado es: ${amigoSorteado}`;
+        // Elimina los valores de la array
+        listaAmigos.splice(amigoSorteado,);
+}
 // Limpia la caja
 function limpiarCaja(elemento){
     document.getElementById(elemento).value = '';
